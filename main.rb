41d04008c9cf9c56d660bf5333543694356b8d96
Michelle Mao <mm321whatsup@gmail.com>
2015-12-03 22:38:35 -0700

    Words will explode at the bottom of the screen;health decrease

--- main.rb
+++ main.rb
@@ -1,6 +1,8 @@
 require 'gosu'
 require_relative "word"
 require_relative "z_order"
+#require_relative "simple_word"
+#require_relative "compound_word"
 
 class GameWindow < Gosu::Window
   def initialize
@@ -8,21 +10,38 @@ class GameWindow < Gosu::Window
     self.caption = "TYPESPEED"
     @background_image = Gosu::Image.new("media/background.jpeg", :tileable => true)
     @font = Gosu::Font.new(20)
-    @words = Array.new 
-    
-
+    @font1= Gosu::Font.new(40)
+    @words = Array.new
+    @words.push(Word.new(600,0, "meep", 1, self))
+    @health = 100
+    @score = 0 
+    @time = Gosu::milliseconds
   end
 
   def update
-    @words.move
+    @words.each { |word|
+    	word.move
+    }
+    @words.each {|word|
+      if word.is_at_bottom?
+        @health -= 10
+      end
+    }
+     @words.each { |word|
+      if word.explode_drawn? == true 
+        @words.delete(word)
+      end
+     }
+    
     
   end
 
   def draw
     @background_image.draw(0, 0, 0)
-    @font.draw("TYPESPEED", 10, 10, ZOrder::UI, 1.0, 1.0, 0xff_00ffff )
-    @word.draw
-
+    @font1.draw("TYPESPEED", 10, 10, ZOrder::UI, 1.0, 1.0, 0xff_00ffff )
+    @font.draw("Score: #{@score}", 10, 50, ZOrder::UI, 1.0, 1.0, 0xff_00ffff)
+	  @font.draw("Health: #{@health}",10, 70 , ZOrder::UI, 1.0, 1.0, 0xff_00ffff)
+    @words.each {|word| word.draw}
   end
 end
 
--- word.rb
+++ word.rb
@@ -1,21 +1,45 @@
 
 class Word
-	attr_reader :x, :y 
-	def initialize(x,y,string,vel)
+	attr_accessor :x, :y 
+	
+
+	def initialize(x,y,string,vel,window)
 		@string = string 
 		@color = Gosu::Color.new(0xff_00ff00)
 		@x = x
 		@y = y
 		@vel = vel 
+		@window = window
+		@explosion = Gosu::Image.new("media/explosion.png")
+		@exploded = false
+		@explode_drawn = false
 		
 	end
 
 	def move
 		@y = @y + @vel 
 	end
+	
 	def draw
-	 @font = Gosu::Font.new(20)
-	 @font.draw(@string, @x, @y, ZOrder::Words, 1.0, 1.0, @color )
+		 @font = Gosu::Font.new(20)
+		 if self.is_at_bottom? == true
+		 	@explode_drawn = true
+		 	@explosion.draw(@x - @explosion.width / 2.0, @y - @explosion.height / 2.0,
+			ZOrder::Words, 1, 1)	
+		else
+			@font.draw(@string, @x, @y, ZOrder::Words, 1.0, 1.0, @color )
+		end
+	end
+
+	def is_at_bottom?
+		if @y == 500
+			@exploded = true
+			return @exploded
+		end
+		
+	end
+	def explode_drawn?
+		return @explode_drawn
 		
 	end
 
