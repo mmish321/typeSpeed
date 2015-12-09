dcb1d4c01678031675d7e639814d8eabf4ec3602
Michelle Mao <mm321whatsup@gmail.com>
2015-12-08 22:48:59 -0700

    Added restart capability and sound effect for explosion

--- main.rb
+++ main.rb
@@ -18,39 +18,32 @@ class GameWindow < Gosu::Window
     @score = 0 
     @time = Gosu::milliseconds
     @beep = Gosu::Sample.new("media/beep.wav")
+    @explosion = Gosu::Sample.new("media/explosion.wav")
    	self.text_input =Gosu::TextInput.new
-    @restart = false
   end
 
   def update
-    @object_words.each { |word|
-    	word.move
-    }
-
-    @object_words.each {|word|
-      if word.is_at_bottom?
-        @health -= 10
-      end
-    }
-
      @object_words.each { |word|
       if word.explode_drawn? == true 
+      	@explosion.play
+      	 @health -= 10
         @object_words.delete(word)
       end
      }
 
-     if Gosu::milliseconds > @time && @time < 60000
+    @object_words.each { |word|
+    	word.move
+    }
+
+    if Gosu::milliseconds> @time && @time <= 60000
       @object_words.push(Word.new(@words[(rand(@words.length-1)).to_i]))
       @time += 3000
-  	 elsif Gosu::milliseconds > @time && @time >= 60000
-  	  @object_words.push(Word.new(@words[(rand(@words.length-1)).to_i]))
+  	elsif Gosu::milliseconds > @time  && @time >= 60000
+      	@object_words.push(Word.new(@words[(rand(@words.length-1)).to_i]))
       @object_words.push(CompoundWord.new(@words[(rand(@words.length-1)).to_i] + @words[(rand(@words.length-1)).to_i]))
-      @time += 6000
-     end
-
-     if @health <= 0
-       @object_words.clear
-     end
+      	@time += 6000
+    end
+    
 
      if Gosu::button_down? Gosu::KbReturn
 	    @object_words.each { |word|
@@ -61,37 +54,51 @@ class GameWindow < Gosu::Window
 	     end
 	    } 
 	    self.text_input = nil 
-			self.text_input =Gosu::TextInput.new 
- 		end
+		self.text_input =Gosu::TextInput.new 
+ 	end
 
-
-  end
-
+ 	if @health <= 0
+     	@object_words.clear
+     end
+ end
   def draw
     @background_image.draw(0, 0, 0)
     @font1.draw("TYPESPEED", 10, 10, ZOrder::UI, 1.0, 1.0, 0xff_00ffff )
     @font.draw("Score: #{@score}", 10, 50, ZOrder::UI, 1.0, 1.0, 0xff_00ffff)
-	  @font.draw("Health: #{@health}",10, 70 , ZOrder::UI, 1.0, 1.0, 0xff_00ffff)
+	@font.draw("Health: #{@health}",10, 70, ZOrder::UI, 1.0, 1.0, 0xff_00ffff)
     @object_words.each {|word| word.draw}
     if @health <= 0
-      @font2.draw("GAME OVER", 300, 250, ZOrder::UI, 1.0, 1.0, 0xff_ff0000)
-      @font2.draw("Final Score : #{@score}", 300, 400, ZOrder::UI, 1.0, 1.0, 0xff_ff0000 )
-      	if Gosu::button_down? Gosu::KbReturn
-      		@restart = true
-      	end
+      @font2.draw("GAME OVER", 300, 150, ZOrder::UI, 1.0, 1.0, 0xff_ff0000)
+      @font2.draw("Final Score : #{@score}", 300, 300, ZOrder::UI, 1.0, 1.0, 0xff_ff0000 )
     end
   end
+  
+ 
  
   def button_down(id)
-    if id == Gosu::KbEscape
-      close
-    end
+     if id == Gosu::KbReturn && @health <= 0
+	    self.caption = "TYPESPEED"
+	    @background_image = Gosu::Image.new("media/background.jpeg", :tileable => true)
+	    @font = Gosu::Font.new(20)
+	    @font1= Gosu::Font.new(40)
+	    @font2 = Gosu::Font.new(100)
+	    data = File.read("words.txt")
+	    @object_words = []
+	    @words = data.split(", ")
+	    @health = 100
+	    @score = 0 
+	    @time = Gosu::milliseconds
+	    @beep = Gosu::Sample.new("media/beep.wav")
+	    @explosion = Gosu::Sample.new("media/explosion.wav")
+	   	self.text_input =Gosu::TextInput.new
+	  elsif id == Gosu::KbEscape
+	  	close
+     end
   end
 end
 
+
+
+
 window = GameWindow.new
 window.show
-if window.restart
-    window = GameWindow.new
-    window.show
-end
--- word.rb
+++ word.rb
@@ -5,15 +5,15 @@ class Word
 	
 	def initialize(string)
 		@string = string 
-		@color = Gosu::Color.new(0xff_000000)
-		@x = rand(1000)
+		@color = Gosu::Color.new(0xff_ff0000)
+		@x = rand(0..1000)
 		@y = 0.0
-		@vel = rand(0.20..1.00)
+		@vel =1.00
 		@explosion = Gosu::Image.new("media/explosion.png")
 		@exploded = false
 		@explode_drawn = false
 		@font = Gosu::Font.new(20)
-		@points = 10 
+		@points = 10
 	end
 
 	def move
@@ -23,17 +23,22 @@ class Word
 	def draw
 		 if self.is_at_bottom? == true
 		 	@explode_drawn = true
-		 	@explosion.draw(@x - @explosion.width / 2.0, @y - @explosion.height / 2.0,
-			ZOrder::Words, 1, 1)	
+		 	
+		 	@explosion.draw(@x - @explosion.width / 2.0, @y - @explosion.height/2.0, 
+			 ZOrder::Words, 1, 1)	
+			
 		else
 			@font.draw(@string, @x, @y, ZOrder::Words, 1.0, 1.0, @color )
 		end
 	end
-
+ 
 	def is_at_bottom?
-		if @y <= 500.0 && @y >= 480.0
+		if @y <= 500.0 && @y >= 475.0
 			@exploded = true
 			return @exploded
+		else
+			 @exploded = false
+			 return @exploded
 		end
 	end
 	
